f595621c694aee33488a75fc7e97a3c1242e2c4c
Osmir <osmirmariano@uft.edu.br>
2017-04-30 19:51:39 -0300

    exemplo 1 e 2 para o minicurso phtread

---
+++ exemplo1.c
@@ -0,0 +1,22 @@
+	#include <stdio.h>
+	#include <pthread.h>
+
+	void *helloWorld() {
+		printf("\nHello World\n\n");
+		pthread_exit(NULL);
+	}
+
+	int main ( ){
+	 	pthread_t thread;
+	 	int flag;;
+
+	 	//Criando uma thread
+	 	flag = pthread_create(&thread, NULL, helloWorld, NULL);
+	 	//Condição para para ocorra um erro ao criar a thread
+	 	if (flag != 0) 
+	 		printf("Erro na criação da nova thread\n");
+
+	 	//Encerrando a thread
+	 	pthread_exit(NULL);
+	 	return 0; /* O programa não vai chegar aqui. */
+	}
